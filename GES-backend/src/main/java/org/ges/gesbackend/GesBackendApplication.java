9f9481626d95a0537a23cf2b3c794824dbb2c5a3
Tymofii Pysarenkov <nfstimoha2004@gmail.com>
2025-05-09 22:16:15 +0300

    init commit

---
+++ GES-backend/src/main/java/org/ges/gesbackend/GesBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ges.gesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GesBackendApplication.class, args);
+    }
+
+}
