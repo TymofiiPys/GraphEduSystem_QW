d759a810e2c3730fe72911c3ea0352965648a083
Tymofii Pysarenkov <nfstimoha2004@gmail.com>
2025-05-09 22:16:15 +0300

    init commit

---
+++ GES-backend/src/test/java/org/ges/gesbackend/GesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ges.gesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
