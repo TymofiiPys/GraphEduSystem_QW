5de62689a1780a729641d3e7ae30e702380f5ae1
Tymofii Pysarenkov <nfstimoha2004@gmail.com>
2025-05-13 19:33:47 +0300

    add bst to backend

--- GES-backend/src/main/java/org/ges/gesbackend/repository/EdgeRepository.java
+++ GES-backend/src/main/java/org/ges/gesbackend/repository/EdgeRepository.java
@@ -1,9 +1,9 @@
 package org.ges.gesbackend.repository;
 
-import org.ges.gesbackend.entity.EdgesId;
+import org.ges.gesbackend.entity.EdgesInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface EdgeRepository extends JpaRepository<EdgesId, UUID> {
+public interface EdgeRepository extends JpaRepository<EdgesInfo, UUID> {
 }
--- GES-backend/src/main/java/org/ges/gesbackend/service/GraphService.java
+++ GES-backend/src/main/java/org/ges/gesbackend/service/GraphService.java
@@ -1,12 +1,13 @@
 package org.ges.gesbackend.service;
 
 import lombok.RequiredArgsConstructor;
+import org.ges.gesbackend.entity.EdgesInfo;
 import org.ges.gesbackend.entity.Graph;
+import org.ges.gesbackend.entity.Node;
 import org.ges.gesbackend.repository.GraphRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +15,69 @@ public class GraphService {
     private final GraphRepository graphRepository;
 
     public Optional<Graph> getGraph(UUID id) {
-        return graphRepository.findById(id);
+        Optional<Graph> graphOptional = graphRepository.findById(id);
+        if (graphOptional.isPresent()) {
+            Graph graph = graphOptional.get();
+            switch (graph.getType()) {
+                case "BST":
+                    Set<Node> nodes = graph.getNodes();
+                    int nanCounter = 0;
+                    for (var node : nodes.stream().filter(x -> {
+                        Map<String, Object> metadata = x.getMetadata();
+                        return ((String) metadata.get("left")).equals("NaN") ||
+                                ((String) metadata.get("right")).equals("NaN")  ;
+                    }).toList()) {
+                        Map<String, Object> metadata = node.getMetadata();
+                        String leftChild = (String) metadata.get("left");
+                        if (leftChild.equals("NaN")) {
+                            String newName = "NaN" + nanCounter++;
+                            metadata.put("left", newName);
+                            Node left = new Node();
+                            left.setGraph(graph);
+                            left.setNodeId(newName);
+                            nodes.add(left);
+                        }
+
+                        String rightChild = (String) metadata.get("right");
+                        if (rightChild.equals("NaN")) {
+                            String newName = "NaN" + nanCounter++;
+                            metadata.put("right", newName);
+                            Node right = new Node();
+                            right.setGraph(graph);
+                            right.setNodeId(newName);
+                            nodes.add(right);
+                        }
+
+                        node.setMetadata(metadata);
+                    }
+                    Set<EdgesInfo> edges = new LinkedHashSet<>();
+                    for (var node : graph.getNodes().stream().filter(x -> !x.getNodeId().startsWith("NaN")).toList()) {
+                        Map<String, Object> metadata = node.getMetadata();
+
+                        EdgesInfo edgeLeft = new EdgesInfo();
+                        edgeLeft.setGraphId(graph);
+                        edgeLeft.setSrcId(node.getNodeId());
+                        edgeLeft.setTgtId((String) metadata.get("left"));
+                        edgeLeft.setEdgeName("");
+
+                        EdgesInfo edgeRight = new EdgesInfo();
+                        edgeRight.setGraphId(graph);
+                        edgeRight.setSrcId(node.getNodeId());
+                        edgeRight.setTgtId((String) metadata.get("right"));
+                        edgeRight.setEdgeName("");
+
+//                        if (!edgeLeft.getTgtId().equals("NaN"))
+                            edges.add(edgeLeft);
+//                        if (!edgeRight.getTgtId().equals("NaN"))
+                            edges.add(edgeRight);
+                    }
+                    graph.setEdges(edges);
+                    break;
+                default:
+                    graph.setType("GRAPH");
+                    break;
+            }
+        }
+        return graphOptional;
     }
 }
